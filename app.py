384089747aa8ba1f847e8d2a6ad165635219d073
unknown <kindudimandiangu@gmail.com>
2024-04-17 18:34:03 +0100

    Initial commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from werkzeug.utils import secure_filename
 #import matricule
 
 app = Flask(__name__)
-
+@app.route("/",methods=["GET"])
+def index():
+    return render_template("index.html")
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="172.16.0.103")
