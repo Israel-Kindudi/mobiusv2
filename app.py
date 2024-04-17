aec3171dd9ec27bcee611fbf30875ecacd8871e1
unknown <kindudimandiangu@gmail.com>
2024-04-17 21:55:28 +0100

    database commit

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request,redirect,url_for,jsonify
 import os
-#import registerDB
+import psycopg2
 from werkzeug.utils import secure_filename
 #import matricule
 import sqlite3
 app = Flask(__name__)
+# Get PostgreSQL database URL from environment variable
+DATABASE_URL = os.environ['postgresql-dimensional-32478']
 # SQLite database initialization
 def create_table():
-    conn = sqlite3.connect('database.db')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS contacts (
@@ -35,8 +37,9 @@ def submit():
         message = request.form['message']
         services = request.form.getlist('services[]')  # Retrieve selected services as a list
 
-        # Save form data to the SQLite database
-        conn = sqlite3.connect('database.db')
+        
+        # Save form data to the PostgreSQL database
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cursor = conn.cursor()
         cursor.execute('INSERT INTO contacts (email, phone, message, services) VALUES (?, ?, ?, ?)',
                        (email, phone, message, ', '.join(services)))
